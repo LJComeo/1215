76ae909c9c57bc7ff2d951ea78d32e1e89eea165
JC555 <572670014@qq.com>
2019-12-15 22:11:06 +0800

    牛牛找工作.cpp

---
+++ 牛牛找工作.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	while (cin >> n >> m)
+	{
+		vector<pair<int, int>> job(n + 1), guy(m);
+		vector<int> map(m);
+		int mx = 0, index = 0, left = 0;
+		job[0] = make_pair(0, 0);
+		for (int i = 1; i <= n; i++)
+		{
+			cin >> job[i].first >> job[i].second;
+		}
+		for (int i = 0; i < m; i++)
+		{
+			cin >> guy[i].first;
+			guy[i].second = i;
+		}
+
+		sort(job.begin(), job.end(), [&](pair<int, int>a, pair<int, int>b)
+		{
+			return a.first < b.first; 
+		});
+		sort(guy.begin(), guy.end(), [&](pair<int, int>a, pair<int, int>b)
+		{
+			return a.first < b.first;
+		});
+		for (int i = 0; i <= m; i++)
+		{
+			mx = max(mx, job[i].second);
+			job[i].second = mx;
+		}
+		while (left < m && index < n + 1)
+		{
+			if (guy[left].first >= job[index].first)
+			{
+				index++;
+			}
+			else
+			{
+				map[guy[left].second] = job[n].second;
+				left++;
+			}
+		}
+		for (int i = 1; i < m; i++)
+		{
+			map[guy[i].second] = job[n].second;
+		}
+		for (auto & i : map)
+		{
+			cout << i << endl;
+		}
+	}
+	return 0;
+}
